bb306f30bf2b0951ddf29e4c1b02ad628af69bef
filip12h <martello2311@gmail.com>
2019-04-11 12:57:17 +0200

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,19 @@
+CFLAGS = -std=c++17 -fconcepts -I../BAgraph/BAgraph/include/
+DBGFLAGS = -g -O0 -pedantic -Wall -Wextra -DBA_GRAPH_DEBUG -D_GLIBCXX_DEBUG
+COMPILE_DBG = $(CXX) $(CFLAGS) $(DBGFLAGS)
+COMPILE_RELEASE = $(CXX) $(CFLAGS)
+
+compile_debug_run: compile_debug
+	./main.out
+
+compile_debug: 
+	$(COMPILE_DBG) main.cpp -o main.out
+
+compile_release: 
+	$(COMPILE_RELEASE) main.cpp -o main.exe
+
+.PHONY: compile_debug_run compile_debug compile_release
+	
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,146 +1,175 @@
 #include <set>
 #include <iostream>
-#include "../BAgraph/impl/basic/include.hpp"
-#include "../BAgraph/io/print_nice.hpp"
-#include "../BAgraph/invariants.hpp"
+#include <basic_impl.hpp>
+#include <io/print_nice.hpp>
+#include <invariants.hpp>
 
 
 using namespace ba_graph;
 using namespace std;
 
+/*
+ * pokus o classu rozsirujucu Graph. Problem s privatnym konstruktorom
+ */
+
+//class MyComponent: public Graph{
+//public:
+//    friend Graph;
+//    int numOfDelEdges;
+//    bool isLarge;
+//    bool isThin;
+//    float delta;
+//
+//
+//};
+
 // program kompilujem nasledujucim prikazom
 // g++ -std=c++17 -fconcepts main.cpp
 
-int main() {
-    Graph g;
+
+    // metoda vrati komponenty grafu
+
+    void DFS(const Rotation &rot, std::map<Vertex, int> &visited, Graph &graph, Factory &f, Graph &g_) {
+        // oznac vrchol ako navstiveny a pridaj ho do komponentu
+        visited[rot.v()] = true;
+        addV(g_, rot.v(), rot.n(), f);
+
+        //kazdy nenavstiveny susedny vrchol pridaj do komponentu aj s hranou ktorou sme ho objavili
+        for (auto &inc: rot) {
+            Rotation& neighbor = inc.r2();
+            if (!visited[neighbor.v()]) {
+                addV(g_, neighbor.v(), neighbor.n(), f);
+                visited[neighbor.v()] = true;
+                DFS(neighbor, visited, graph, f, g_);
+            }
+        }
+    }
+
+    vector<Graph> connectedComponents(Graph &g, Factory &f) {
+        vector<Graph> components;
+        std::map<Vertex, int> visited;
+        for(auto &rot: g) visited[rot.v()]=0;
+        Graph graph(createG(f));
+
+        //najdeme nenavstiveny vrchol a nasledne najdeme hrany a vrcholy s nim spojene
+        for (auto &rot: g) {
+            if (!visited[rot.v()]) {
+                DFS(rot, visited, g, f, graph);
+                for(auto &rot1: graph)
+                    for(auto it: g[rot1.n()].list(IP::primary()))
+                        if(graph.contains(it->n2()))
+                            addE(graph, rot1, it->r2(), it->e(), f);
+
+                components.push_back(std::move(graph));
+            }
+        }
+        return components;
+    }
+
+    /*
+     * vyhadzujeme male sety v ktorcyh sa nachadzaju cierne cykly
+     */
+    void step1(float epsilon, int numOfVertices, vector<Graph> &rodinaKomponentov){
+        for (auto &g: rodinaKomponentov){
+            if (g.order()<=20/epsilon+41){
+                int b1 = 0, b3 = 0;
+                //TODO: prejdeme vrcholmi a spocitame pocet vrcholov s 3 ciernymi hranami a s 1 ciernou hranou
+                // ak sa rovnaju, tak komponent ma evidentne cyklus
+                for (auto &rot1: g){
+                    int pocetCiernychHran = 0;
+                    for(auto it: g[rot1.n()].list(IP::primary()))
+                        if(g.contains(it->n2()))
+                            pocetCiernychHran++;
+                    assert((pocetCiernychHran>0)&&(pocetCiernychHran<=3));
+                    if (pocetCiernychHran==1)
+                        b1++;
+                    if (pocetCiernychHran==3)
+                        b3++;
+                }
+                if (b1==b3)
+                    rodinaKomponentov.erase(remove(rodinaKomponentov.begin(), rodinaKomponentov.end(), g));
+            }
+        }
+    }
+    /*
+     * vyhadzujeme velke tenke sety aj s malymi setmi cervenou hranou s nimi spojenymi
+     */
+    void step2(float epsilon, Graph graph, vector<Graph> &rodinaKomponentov){
+        //z rodiny komponentov budeme vyhadzovat prvky pokym tam budu, pricom vyhadzoavnim sa mozu zmenit tenke mnoziny
+        // na hrube a naopak, preto prehladavanie ukoncime az ked od posledneho najdenia neprejdeme vsetky prvky
+        int esteTrebaPrejst = rodinaKomponentov.size();
+        for (auto &g: rodinaKomponentov){
+
+        }
+        while(true){
+            bool vyhodiliSme = 0;
+            for (auto &g: rodinaKomponentov){
+                //TODO: pokial sme pocas celeho for cyklu nevyhodili nic, prejdeme do riadku kde brejkneme while cyklus.
+                // pokial sme vyhodili, tak v tom momente sa vraciame na zaciatok a robime cely for cyklus od zaciatku
+                if ("vyhadzujeme"){
+                    //vyhodime z rodiny komponentov aj so vsetkymi dalsimi co treba
+                    vyhodiliSme = true;
+                }
+            }
+            if (!vyhodiliSme)
+                break;
+        }
+
+    }
+
+/*
+* funkcia z grafu, v ktorom su cervene a cierne hrany take, ze kazdeho vrcholu sa dotyka aspon jedna cierna hrana
+* a zaroven nech je cervenych hran viac ako ciernych.
+* Najdeme mnozinu urcitej/obmedzenej velkosti taku, ze cervenych interncyh hran je viac ako ciernych externych
+*/
+void redBlackEdges(int numOfVertices, vector<Edge> redEdges, vector<Edge> blackEdges) {
+    assert(redEdges.size()>numOfVertices/2); //podla lemy musi byt pocet cervenych hran vacsi ako pocet vrcholov grafu zmenseny o polovicu
+    float epsilon = 0; // nesko mu priradime kladnu hodnotu taku, aby |R|>|V|(1/2+epsilon)
+
+    while ((epsilon<=0)||(epsilon>=1/2)) {
+        cout<< "Zvoľ hodnotu epsilon z intervalu (0, 1/2)\n";//pozn: prilis velke alebo male epsilon nam nemusia vyhovovat
+        cin >> epsilon;
+    }
     Factory f;
+    Graph povodnyGraf(createG(f));
+    Graph ciernyGraf(createG(f));
 
-    for(int i=0;i<10;i++) addV(g, i);
-    for(int i=0;i<9;i++) addE(g, Location(i, i+1), f);
-    addE(g, Location(9,0), f);
+    addMultipleV(ciernyGraf, numOfVertices, f);
+    addMultipleE(ciernyGraf, blackEdges, f);
+    addMultipleV(povodnyGraf, numOfVertices, f);
+    addMultipleE(povodnyGraf, blackEdges, f);
+    addMultipleE(povodnyGraf, redEdges, f);
 
-    //g[5].find(IP::all(), IT::v2());
-    //g[5].find(IT::v2());
+    vector<Graph> rodinaKomponentov = connectedComponents(ciernyGraf, f);
+    //z rodinaKomponentov budeme neskor vyhadzovat jednotlive elementy, zostane tam velka tenka
+    //mnozina s nejakymi vlastnostami a nasledne najdeme mnozinu splnajucu lemu
+
+    float delta = epsilon/(1+2*epsilon);
+
+    //TODO: prejdeme si rodinaKomponentov a male sety
+
+
+
+}
+
+int main() {
+    Factory f;
+    Graph g(createG(f));
+
+    for(int i=0;i<8;i++) addV(g, i, f);
+    for(int i=0;i<3;i++) addE(g, Loc(i, i+1), f);
+    addE(g, Loc(3,0), f);
+    for(int i=4;i<6;i++) addE(g, Loc(i, i+1), f);
+
+    //g[5].list(IP::all(), IT::v2());
 
 
 
     std::cout<<g;
+    vector<Graph> sg = connectedComponents(g, f);
 
-
-class Lemma1 {
-    /*
- * funkcia z grafu, v ktorom su cervene a cierne hrany take, ze kazdeho vrcholu sa dotyka aspon jedna ciarna hrana
- * a zaroven nech je cervenych hran viac ako ciernych.
- * Najdeme mnozinu urcitej/obmedzenej velkosti taku, ze cervenych interncyh hran je viac ako ciernych externych
- */
-    void redBlackEdges(set<Vertex> vertices, set<Edge> redEdges, set<Edge> blackEdges) {
-
-    }
-
-    // metoda vrati komponenty grafu
-//    set<Graph> connectedComponents(set<Vertex> vertices, set<Edge> edges) {
-//        int vs = vertices.size();
-//        set<Graph> components;
-//        set<Vertex> visited;
-//        //najdeme nenavstiveny vrchol a nasledne najdeme hrany a vrcholy s nim spojene
-//        for (Vertex v : vertices) {
-//            bool is_in = visited.find(v) != visited.end();
-//            if (!is_in) {
-//
-//                Graph graph;
-//
-//                graph = DFS(vertices, edges, v, visited);
-//
-//                components.insert(graph);
-//            }
-//        }
-//    }
-//
-//    Graph DFS(set<Vertex> vertices, set<Edge> edges, Vertex &v, set<Vertex> visited) {
-//        // oznac vrchol ako navstiveny
-//        visited[v] = true;
-//        Graph graph;
-//        addV(graph, v);
-//
-//        //cout << v << " ";
-//
-//        // prejdeme vsetkymi vrcholmi spojenymi s vrcholom
-//        list<int>::iterator i;
-//        for ()
-//            if (!visited[*i])
-//                DFSUtil(*i, visited);
-//    }
-
-        // metoda vrati komponenty grafu
-public:
-//        set<Graph> connectedComponents(int vertices, Graph &g) {
-//            set<Graph> components;
-//            bool *visited = new bool[vertices];
-//
-//            //G[n].find(IP::all(),IT::n2())
-//
-//            for (int i = 0; i < vertices; i++)
-//                visited[i] = false;
-//            //najdeme nenavstiveny vrchol a nasledne najdeme hrany a vrcholy s nim spojene
-//            for (int i = 0; i < vertices; i++) {
-//                if (!visited[i]) {
-//
-//                    Graph graph;
-//
-//                    graph = DFS(vertices, i, visited, g);
-//
-//                    components.insert(graph);
-//                }
-//            }
-//            return components;
-//        }
-
-        vector<Graph> connectedComponents(int vertices, Graph &g) {
-            vector<Graph> components;
-            bool *visited = new bool[vertices];
-
-            //G[n].find(IP::all(),IT::n2())
-
-            for (int i = 0; i < vertices; i++)
-                visited[i] = false;
-            //najdeme nenavstiveny vrchol a nasledne najdeme hrany a vrcholy s nim spojene
-            for (int i = 0; i < vertices; i++) {
-                if (!visited[i]) {
-
-                    Graph graph;
-
-                    graph = DFS(vertices, i, visited, g);
-
-                    components.push_back(graph);
-                }
-            }
-            return components;
-        }
-
-        Graph DFS(int vertices, int v, bool visited[], Graph &graph) {
-            // oznac vrchol ako navstiveny a pridaj ho do komponentu
-            visited[v] = true;
-            Graph g_;
-            Factory factory;
-            addV(g_, v);
-
-            //kazdy nenavstiveny susedny vrchol pridaj do komponentu aj s hranou ktorou sme ho objavili
-            for (int i = 0; i < graph[v].degree(); i++) {
-                int neighbor = graph[v][i].n2().n; //funkciou .n  spravime z Number int
-                if (!visited[neighbor]) {
-                    addV(g_, neighbor, factory);
-                    visited[neighbor] = true;
-                    DFS(vertices, neighbor, visited, graph);
-                }
-            }
-            return g_;
-        }
-
-    };
-
-    Lemma1 l;
-    vector<Graph> sg = l.connectedComponents(10, g);
-
-    //cout<<sg;
+    while (!sg.empty())
+    {cout<<sg.back();
+    sg.pop_back();}
 
 };
